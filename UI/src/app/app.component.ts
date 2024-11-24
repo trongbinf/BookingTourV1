4b12f7038cd99444b4256f93c2f8b000cc8e37a3
Nguyễn Xuân Tâm <xuantamjee413@gmail.com>
2024-11-24 14:44:09 +0700

    add admin sidebar

--- UI/src/app/app.component.ts
+++ UI/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./core/navbar/navbar.component";
 import { FooterComponent } from "./core/footer/footer/footer.component";
+import { AdminSidebarComponent } from './core/admin-sidebar/admin-sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, AdminSidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ UI/src/app/core/admin-sidebar/admin-sidebar.component.ts
@@ -0,0 +1,27 @@
+import { routes } from './../../app.routes';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../features/auth/services/auth.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Router, NavigationEnd } from '@angular/router';
+
+
+@Component({
+  selector: 'app-admin-sidebar',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './admin-sidebar.component.html',
+  styleUrl: './admin-sidebar.component.css'
+})
+export class AdminSidebarComponent {
+  showSidebar: boolean = false;
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.showSidebar = event.url.startsWith('/admin');
+      }
+    });
+  }
+}
